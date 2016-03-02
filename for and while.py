f6cfe1d470fcec07e89f0dfba17dcb7c6b2be1a2
Harry-Yao <673441990@qq.com>
2016-03-02 21:41:30 +0800

    first push

---
+++ for and while.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+L = ['Bart', 'Lisa', 'Adam']
+
+for i in L :
+    print(i)
+
+d = {
+    'Michael': 95,
+    'Bob': 75,
+    'Tracy': 85
+}
+print('d[\'Michael\'] =', d['Michael'])
+print('d[\'Bob\'] =', d['Bob'])
+print('d[\'Tracy\'] =', d['Tracy'])
+print('d.get(\'Thomas\', -1) =', d.get('Thomas', -1))
---
+++ funtions.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+import math
+'''
+n1 = 255
+n2 = 1000
+
+print(hex(n1),hex(n2))
+'''
+def quadratic(a, b, c):
+    return (math.sqrt(b**2-4*a*c)-b)/(2*a),-(b+math.sqrt(math.pow(b,2)-4*a*c))/(2*a)
+
+print(quadratic(2, 3, 1)) # => (-0.5, -1.0)
+print(quadratic(1, 3, -4)) # => (1.0, -4.0)
